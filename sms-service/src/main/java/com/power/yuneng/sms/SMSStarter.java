e7445656c25e4b257b780ddce47953e9891e2078
wuhun0301 <wuhun0301@qq.com>
2017-07-27 16:44:44 +0800

    init

--- sms-service/src/main/java/com/power/yuneng/sms/SMSStarter.java
+++ sms-service/src/main/java/com/power/yuneng/sms/SMSStarter.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -12,9 +14,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.SocketAddress;
 
 
 /**
@@ -24,7 +23,7 @@ import java.net.SocketAddress;
 @MapperScan("com.power.yuneng.sms.dao")
 @ImportResource("classpath:spring/dubbo.xml")
 @EnableTransactionManagement
-public class SMSStarter {
+public class SMSStarter{
     // 启动的时候要注意，由于我们在controller中注入了RestTemplate，所以启动的时候需要实例化该类的一个实例
     @Autowired
     private RestTemplateBuilder builder;
@@ -42,7 +41,6 @@ public class SMSStarter {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(SMSStarter.class, args);
-        System.in.read();
     }
 }
 
